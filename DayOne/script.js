67f78f5a31c5c3249d54e5b110d5145eaefce433
RashiChouksey12 <rashi.chouksey@successive.tech>
2021-11-10 18:26:32 +0530

    first commit

---
+++ DayOne/script.js
@@ -0,0 +1,59 @@
+console.log("How to use Console .log")
+
+// varible stored in mamory comp can be redeclear 
+
+var x = 100;
+console.log(x)
+
+
+var a = 10;
+var a = 20;
+console.log(a)
+
+// Let cannot redeclear 
+let p =1 ;
+console.log(p)
+
+let q = 11;
+// Let cannot redeclear   let q = 2 ;
+console.log(q)
+
+
+// Const constent value cannot redeclear reassign
+const r = 5 ;
+console.log(r)
+
+
+
+
+// ---------------DataType-------------
+// Totle Premitive Datatype==7
+// *String * Number * Boolean * undefined *Symbol *null * BigInt
+
+var str ='This is sring';
+console.log(typeof str)
+console.log(str)
+
+var Number =10;
+console.log(typeof Number)
+console.log(Number)
+
+var Boolean =true;
+console.log(typeof Boolean)
+console.log(Boolean)
+
+// Totle NonPremitive Datatype
+//object array regexp
+
+var obj ={
+    a:100,
+    b:"hii"
+}
+console.log(obj.a)
+console.log(obj.b)
+
+var ary =[1,2,3,4,5,'Rashi'];
+console.log(ary[1],ary[0],ary[5])
+
+
+
---
+++ DayTwo/script.js
@@ -0,0 +1,20 @@
+
+
+function computeage(cAge , dob)
+{
+  let age = cAge-dob
+  return age
+}
+
+
+var get =computeage(2021 ,1998);
+console.log(get);
+
+
+function computEbmi(height , weight)
+{
+  let bmi = weight/(height*height)
+  return bmi
+}
+var get =computEbmi(15 ,50);
+console.log(get);
