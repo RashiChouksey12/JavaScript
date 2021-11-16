85b905b4682de72b373bd9b7572268f0f31a6cc0
Rashi Chouksey <rashi.chouksey@rashir251.successivetech.com>
2021-11-16 20:21:19 +0530

    task3 done

--- DayFive/task3.js
+++ DayFive/task3.js
@@ -1,1 +1,16 @@
+var Age = {
+    Poorti: 28,
+    findAge: function()
+    {
+     if (Age.Poorti>=18)
+     console.log("Poorti is adult")
+     else{
+        console.log("Poorti is teenage")
+     }
+    }
+};
+console.log(Age.findAge())
+
+
+
 
